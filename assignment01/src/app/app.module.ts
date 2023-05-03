7997f9e25a54fd63d776ea5b65e13e797b01aa23
Dakshendra Singh <pal.dakshendra80@gmail.com>
2023-05-03 15:31:31 +0530

    Assignment 01
    2 component :- success alert, warning alert
    create component using cli commands (ng g c successalert) ,
    and other component add manually ,
    add bootstap   ,
    output and warning message in the component
    
    Angular - complete guide 2023
    Instructor:- Maximilian Schwarzmüller

---
+++ assignment01/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningalertComponent,
+    SuccessalertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
